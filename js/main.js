0e64ecfd141e61ef4fb5fbb5184c3a6ca3af8d01
Roman3341 <fearik435@gmail.com>
2021-10-05 13:29:11 +0300

    2 первые функции

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+getRandomIntInclusive(1, 10);
+
+//Функция для проверки максимальной длины строки.
+function getStringLength (string, maxLength) {
+  const stringLength = string < maxLength ? true : false;
+  return stringLength;
+}
